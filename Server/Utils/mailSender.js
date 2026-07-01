c8691baa34a90e5e247a80ed3f9f9214acbd4aeb
Sudarshan Sharma <your-github-sudarshanpandat@gmail.com>
2026-07-02 01:08:03 +0530

    commit done changer it

--- Server/Utils/mailSender.js
+++ Server/Utils/mailSender.js
@@ -1,30 +1,25 @@
 const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 exports.mailSender = async (email, title, body) => {
+  const defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+  const apiKey = defaultClient.authentications["api-key"];
+  apiKey.apiKey = process.env.BREVO_API_KEY;
+
+  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+  const sendSmtpEmail = {
+    sender: {
+      email: process.env.MAIL_FROM,
+      name: "StudyNotion",
+    },
+    to: [{ email }],
+    subject: title,
+    htmlContent: body,
+  };
+
   try {
-    const defaultClient = SibApiV3Sdk.ApiClient.instance;
-
-    // Brevo API Key
-    const apiKey = defaultClient.authentications["api-key"];
-    apiKey.apiKey = process.env.BREVO_API_KEY;
-
-    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-
-    const sendSmtpEmail = {
-      sender: {
-        email: process.env.MAIL_FROM,
-        name: "StudyNotion",
-      },
-
-      to: [{ email }],
-
-      subject: title,
-
-      htmlContent: body,
-    };
-
     const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
-
     console.log("✅ Email sent successfully");
     return data;
   } catch (error) {
